e47f556ac269967a011d40b0a9dc9ddcf70d9005
hyungjunlee <hjlee1765@gmail.com>
2020-07-11 01:12:31 +0900

    update

---
+++ AsyncConsoleTest/AsyncConsoleTest/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleTest
+{
+
+	/*
+	1. await는 nonblocking async 방식인가?
+	2. await 2개가 있을때 아래 await 메서드는 실행이 안되나?
+	3. whenall 과 waitall 의 차이가 무엇인가?
+	4. task.result는 어디서기다리나? (동기인가 비동기인가?)
+
+	 *  */
+
+	class Program
+	{
+
+		public static async Task<string> getAsync()
+		{
+			ServicePointManager.Expect100Continue = true;
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+			var client = new HttpClient();
+			Task<string> getStringTask = client.GetStringAsync("http://docs.microsoft.com/dotnet");
+
+			string urlContents = await getStringTask;
+
+			Console.WriteLine("exec - getAsync / thread : " + Thread.CurrentThread.ManagedThreadId);
+			return urlContents.Length.ToString();
+		}
+		public static async Task<string> getAsync2()
+		{
+			ServicePointManager.Expect100Continue = true;
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+			var client = new HttpClient();
+			Task<string> getStringTask = client.GetStringAsync("http://docs.microsoft.com/dotnet");
+
+			string urlContents = await getStringTask;
+
+			Console.WriteLine("exec - getAsync2 / thread : " + Thread.CurrentThread.ManagedThreadId);
+			return urlContents.Length.ToString() + "2";
+		}
+
+		public static async Task<string> doSomething()
+		{
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			var res = getAsync();
+
+			var res2 = getAsync2();
+
+			await Task.WhenAll(res, res2);
+
+			stopWatch.Stop();
+
+			Console.WriteLine("exec - doSomething / thread : " + Thread.CurrentThread.ManagedThreadId);
+			Console.WriteLine(stopWatch.Elapsed.ToString());
+			return res.Result + res2.Result;
+			//return res + res2;
+		}
+
+		static void Main(string[] args)
+		{
+			var res = doSomething().GetAwaiter().GetResult();
+			//var res = doSomething().GetAwaiter().GetResult();
+
+			Console.WriteLine("exit - main / res : " + res);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ AsyncWebApiTest/AsyncWebApiTest/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AsyncWebApiTest.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index() {
+
+			return Content("hello world");
+		}
+		[HttpGet]
+		public async Task<String> Main()
+		{
+			var res = await new HomeBiz().getAsync();
+
+			Debug.WriteLine("exit - Main" + Thread.CurrentThread.ManagedThreadId);
+
+			return res;
+		}
+	}
+
+	public class HomeBiz
+	{
+		public async Task<string> getAsync()
+		{
+			ServicePointManager.Expect100Continue = true;
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+			var client = new HttpClient();
+			Task<string> getStringTask = client.GetStringAsync("http://docs.microsoft.com/dotnet");
+
+			string urlContents = await getStringTask;
+
+			Debug.WriteLine("exit - AccessTheWebAsnyc()" + Thread.CurrentThread.ManagedThreadId);
+			return urlContents.Length.ToString();
+		}
+	}
+
+}

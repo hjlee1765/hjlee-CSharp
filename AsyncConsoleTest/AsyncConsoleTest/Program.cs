ef1ecbce878b8b4d30868f83d23509005a06e5ac
hjlee <hjlee1765@gmail.com>
2020-07-19 23:57:27 +0900

    controller에서 await를 만나면?

--- AsyncConsoleTest/AsyncConsoleTest/Program.cs
+++ AsyncConsoleTest/AsyncConsoleTest/Program.cs
@@ -12,9 +12,11 @@ namespace AsyncConsoleTest
 {
 
 	/*
-	1. await는 nonblocking async 방식인가?
-	2. await 2개가 있을때 아래 await 메서드는 실행이 안되나?
-	3. whenall 과 waitall 의 차이가 무엇인가?
+	1. await는 nonblocking async 방식인가? -> 물론. await 의 호출자에게 제어권을 반환한다.
+	2. await 2개가 있을때 아래 await 메서드는 실행이 안되나? -> 첫번째 await에 걸리면 호출자로 제어권이 반환되기에 await아래에 있는 await는 결국
+															첫번째 await가 끝나야 두번째 await 메서드가 실행된다. 
+	3. whenall 과 waitall 의 차이가 무엇인가? -> whenall : nonblocking 이다. 제어권을 호출자로 넘긴다.
+											-> waitall : blocking이다.
 	4. task.result는 어디서기다리나? (동기인가 비동기인가?)
 
 	 *  */
@@ -42,7 +44,7 @@ namespace AsyncConsoleTest
 
 			var client = new HttpClient();
 			Task<string> getStringTask = client.GetStringAsync("http://docs.microsoft.com/dotnet");
-
+			
 			string urlContents = await getStringTask;
 
 			Console.WriteLine("exec - getAsync2 / thread : " + Thread.CurrentThread.ManagedThreadId);
@@ -70,10 +72,10 @@ namespace AsyncConsoleTest
 
 		static void Main(string[] args)
 		{
-			var res = doSomething().GetAwaiter().GetResult();
+			var res = doSomething();
 			//var res = doSomething().GetAwaiter().GetResult();
 
-			Console.WriteLine("exit - main / res : " + res);
+			Console.WriteLine("exit - main / res : " + res + " thread : " + Thread.CurrentThread.ManagedThreadId);
 			Console.ReadLine();
 		}
 	}
--- AsyncWebApiTest/AsyncWebApiTest/Controllers/HomeController.cs
+++ AsyncWebApiTest/AsyncWebApiTest/Controllers/HomeController.cs
@@ -11,26 +11,84 @@ using System.Threading;
 
 namespace AsyncWebApiTest.Controllers
 {
+
+	/*
+	 * API 안에서 여러 시간걸리는 작업(A,B)을 호출해야 한다 하자.
+	 * 
+	 * A,B를 비동기로 각각 쏘고, whenAll을 하더라도 결국 호출자인 Controller까지 제어권이 넘어오게되며
+	 * controller에서 await을 만나면 해당 쓰레드를 중단시키고, 제어권으로 또다른 스레드를 실행시키게 하는걸까?
+	 *
+	 * -> controller에서 await을 만나면, asp.net request thread pool. 
+	 *   즉, 이 서버는 더 많은 request를 처리할수 있게 되는 것이다
+	 *
+	 *	controller가 async라면 return하기 전, 무조건 await가 필요하다.
+	 *	api의 결과를 즉시 리턴하여 view로 먼저 뿌려주고, 오래 걸리는 작업(A,B)의 결과를 받아서 다시 view로 뿌릴 수 없다.
+	 *  즉, async/await로 HTTP request-response protocol을 깰 수 없다.
+	 *  
+	 *  결과를 즉시 리턴시키고 싶다면, background thread에 큐잉을 했다가 client가 주기적으로 체크해서 결과를 가져오는 방식으로 해야될까???
+	 *  -> 토비가 언급했었는데 그새 까먹으.. deferredResult 큐 확인해야겠다.
+	 
+	 */
+
 	public class HomeController : Controller
 	{
 		public ActionResult Index() {
 
 			return Content("hello world");
 		}
+
+		//Async Controller는 await만나면 ThreadPool을 더 자유롭게 만든다.
 		[HttpGet]
 		public async Task<String> Main()
 		{
-			var res = await new HomeBiz().getAsync();
-
-			Debug.WriteLine("exit - Main" + Thread.CurrentThread.ManagedThreadId);
+			var res = await new HomeBiz().doSomething();
 
 			return res;
+		}
+
+		//결과 즉시 리턴시, controller는 async 메서드가 될 수 없다.
+		[HttpGet]
+		public ActionResult CompleteImmediatelyReturn2()
+		{
+			var res = new HomeBiz().doSomething();
+			return Content("Complete Immediately Return : " + res.ToString());
 		}
 	}
 
 	public class HomeBiz
 	{
+		public async Task<string> doSomething()
+		{
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			var res = getAsync();
+
+			var res2 = getAsync2();
+			await Task.WhenAll(res, res2);
+
+			stopWatch.Stop();
+
+			Debug.WriteLine("exec - doSomething / thread : " + Thread.CurrentThread.ManagedThreadId);
+			Debug.WriteLine(stopWatch.Elapsed.ToString());
+			return res.Result + res2.Result;
+			//return res + res2;
+		}
+
 		public async Task<string> getAsync()
+		{
+			ServicePointManager.Expect100Continue = true;
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+			var client = new HttpClient();
+			Task<string> getStringTask = client.GetStringAsync("http://docs.microsoft.com/dotnet");
+			string urlContents = await getStringTask;
+
+			Debug.WriteLine("exit - AccessTheWebAsnyc()" + Thread.CurrentThread.ManagedThreadId);
+			return urlContents.Length.ToString();
+		}
+
+		public static async Task<string> getAsync2()
 		{
 			ServicePointManager.Expect100Continue = true;
 			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
@@ -40,8 +98,8 @@ namespace AsyncWebApiTest.Controllers
 
 			string urlContents = await getStringTask;
 
-			Debug.WriteLine("exit - AccessTheWebAsnyc()" + Thread.CurrentThread.ManagedThreadId);
-			return urlContents.Length.ToString();
+			Debug.WriteLine("exec - getAsync2 / thread : " + Thread.CurrentThread.ManagedThreadId);
+			return urlContents.Length.ToString() + "2";
 		}
 	}
 
